6cfcb643d035e14c88e38ec08dafc9012d0f01c7
chanfficial <official2930@naver.com>
2025-08-27 11:17:37 +0900

    footer 부분 구현

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -77,31 +77,28 @@ const Landing: React.FC = () => {
         </div>
       </section>
 
+      {/* 메인 컨텐츠 */}
+      <section className="pb-[72px]"></section>
+
       {/* 푸터 */}
-      <footer className="bg-white py-16 text-gray-700">
-        {/* 중앙: 회사 정보 */}
-        <div className="mb-8 border-t border-gray-200 pt-8">
-          <div className="md:items-right flex flex-col items-start justify-between md:flex-row">
-            <div className="items-right mb-4 flex space-x-3 md:mb-0">
-              <div className="items-right justify-right flex h-12 w-12">
-                <img
-                  src="/wise-footer-logo.png"
-                  alt="WiseON Footer Logo"
-                  className="h-full w-full object-contain"
-                />
-              </div>
-            </div>
-            <div className="text-right text-sm text-gray-600">
-              <p className="text-sm text-gray-600">
-                대표이사 : 김원표, 사업자등록번호 : 113-86-13917
-              </p>
-              <p>
-                서울특별시 강남구 언주로 309 기성빌딩 3층 (주)와이즈인컴퍼니
-              </p>
-              <p>wic@wiseinc.co.kr</p>
-              <p className="font-semibold">고객센터 02.558.5144</p>
-            </div>
+      <footer className="w-full border-t border-gray-300 bg-white py-10 text-gray-700">
+        <div className="mx-auto flex w-[1280px] flex-col">
+          <img
+            src="/wise-footer-logo.png"
+            alt="와이즈인 컴퍼니 푸터 로고"
+            className="w-[42px]"
+          />
+          <div className="mt-[7px] flex flex-col gap-1 text-xs text-[#818181]">
+            <p>대표이사 : 김원표, 사업자등록번호 : 113-86-13917</p>
+            <p>
+              서울특별시 강남구 언주로 309 기성빌딩 3층 ㈜와이즈인컴퍼니
+              wic@wiseinc.co.kr
+            </p>
+            <p>고객센터 02.558.5144</p>
           </div>
+          <p className="mt-2.5 text-xs text-[#818181]">
+            Copyright WISEINCOMPANY CO.,LTD ALL Right Reserved
+          </p>
         </div>
       </footer>
     </div>
