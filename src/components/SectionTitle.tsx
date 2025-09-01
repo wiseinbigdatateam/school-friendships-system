bb4068eb048dc9604e6d9ee4e1812c898c5638b9
chanfficial <72565344+chaeeunj@users.noreply.github.com>
2025-09-01 15:03:19 +0900

    랜딩 페이지: SectionTitle 컴포넌트에 반응형 적용

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -5,10 +5,10 @@ type DescType = {
 
 function SectionTitle({ title, desc }: { title: string; desc?: DescType }) {
   return (
-    <div className="flex flex-col items-center gap-2">
-      <h2 className="text-2xl font-semibold">{title}</h2>
+    <div className="flex flex-col items-center gap-2 max-md:items-start max-md:gap-1">
+      <h2 className="text-2xl font-semibold max-md:text-lg">{title}</h2>
       {desc && (
-        <p className="flex flex-col items-center">
+        <p className="flex flex-col items-center max-md:items-start max-md:text-sm">
           <span className="font-semibold text-sky-700">{desc.point}</span>
           {desc.basic}
         </p>
