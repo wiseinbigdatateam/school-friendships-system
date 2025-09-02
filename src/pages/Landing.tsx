9e72a8f5464d85a48f3bb36a64be7aa802e6bd70
chanfficial <72565344+chaeeunj@users.noreply.github.com>
2025-09-02 09:53:36 +0900

    랜딩 페이지: Footer 컴포넌트 분리 후 적용 & 반응형 적용

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import LandingHeader from "../components/landing/LandingHeader";
+import Footer from "../components/landing/Footer";
 import TabMenu from "../components/landing/TabMenu";
 import PeerRelationShipsAndDiagnosticAssessment from "../components/landing/TabContent/PeerRelationShipsAndDiagnosticAssessment";
 import AiDiagnosisReport from "../components/landing/TabContent/AiDiagnosisReport";
@@ -63,27 +64,7 @@ const Landing: React.FC = () => {
         )}
       </section>
 
-      {/* 푸터 */}
-      <footer className="w-full border-t border-gray-300 bg-white py-10 text-gray-700">
-        <div className="mx-auto flex w-[1280px] flex-col">
-          <img
-            src="/wise-footer-logo.png"
-            alt="와이즈인 컴퍼니 푸터 로고"
-            className="w-[42px]"
-          />
-          <div className="mt-[7px] flex flex-col gap-1 text-xs text-[#818181]">
-            <p>대표이사 : 김원표, 사업자등록번호 : 113-86-13917</p>
-            <p>
-              서울특별시 강남구 언주로 309 기성빌딩 3층 ㈜와이즈인컴퍼니
-              wic@wiseinc.co.kr
-            </p>
-            <p>고객센터 02.558.5144</p>
-          </div>
-          <p className="mt-2.5 text-xs text-[#818181]">
-            Copyright WISEINCOMPANY CO.,LTD ALL Right Reserved
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
