7e3c5847c7ab9d78a68069a1e7aa9ecb08933f62
chanfficial <72565344+chaeeunj@users.noreply.github.com>
2025-09-09 10:46:25 +0900

    설문 리스트: 검색창 정렬 위치 수정, 설문 리스트 n개씩 렌더링하는 셀렉트 옵션 위치 페이지네이션 옆으로 이동

--- src/pages/SurveyManagement.tsx
+++ src/pages/SurveyManagement.tsx
@@ -1281,9 +1281,9 @@ const SurveyManagement: React.FC = () => {
       )}
 
       {/* 컨트롤 패널 */}
-      <div className="mb-4 rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
-        <div className="flex flex-col items-start justify-between gap-4 sm:flex-row sm:items-center">
-          <div className="flex flex-1 flex-col gap-4 sm:flex-row">
+      <div className="mb-4 rounded-lg border border-gray-200 bg-white p-5 shadow-sm">
+        <div className="flex flex-col items-end justify-between gap-4 sm:flex-row sm:items-center">
+          <div className="flex flex-1 flex-col justify-end gap-4 sm:flex-row">
             {/* 검색 */}
             <div className="relative">
               <input
